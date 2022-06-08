79fef555c3cb700dd9c4f5978b2653385dbdb447
krish91221 <muralikrish.tech@gmail.com>
2022-06-08 23:06:11 +0530

    new commit

--- src/test/java/com/crm/practice/GridSample.java
+++ src/test/java/com/crm/practice/GridSample.java
@@ -3,17 +3,18 @@ package com.crm.practice;
 import java.net.MalformedURLException;
 import java.net.URL;
 
-import org.openqa.selenium.Capabilities;
-import org.openqa.selenium.remote.DesiredCapabilities;
+ import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.Test;
+
 
 public class GridSample {
-	public static void main(String[] args) throws MalformedURLException {
-		URL url=new URL("https://192.168.0.114:4444/wd/hub");
+	@Test
+	public void launch()throws MalformedURLException {
+		URL url=new URL("http://localhost:4444/wd/hub");
 		DesiredCapabilities cap=new DesiredCapabilities();
-		cap.setBrowserName("firefox");
+		cap.setBrowserName("chrome");
 		RemoteWebDriver driver=new RemoteWebDriver(url,cap);
-		driver.get("https://www.facebook.com");
-
+		driver.get("http://www.facebook.com");
 	}
 }
--- src/test/java/com/crm/practice/SeleniumGridPractice.java
+++ src/test/java/com/crm/practice/SeleniumGridPractice.java
@@ -3,37 +3,34 @@ package com.crm.practice;
 import java.io.IOException;
 import java.net.URL;
 
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
-@Parameters("browser")
+
 public class SeleniumGridPractice {
-	RemoteWebDriver driver;
-	@BeforeClass
-	public void launchBrowserInRemoteachine(String browser) throws IOException
+	@Parameters("browser")
+	@Test
+	public void launchBrowserInRemoteachine(String browser) throws IOException, InterruptedException
 	{
-		URL url=new URL("https://192.168.43.101:4444/wd/hub");
+		RemoteWebDriver driver;
+		URL url=new URL("http://192.168.0.114:4444/wd/hub");
 		DesiredCapabilities cap=new DesiredCapabilities();
+		cap.setPlatform(Platform.WINDOWS);
 		if(browser.equals("chrome"))
 		{
-			//cap.setPlatform(windows);
 			cap.setBrowserName("chrome");
 		}
 		else if(browser.equals("firefox"))
 		{
-
-			cap.setBrowserName("chrome");
+			cap.setBrowserName("firefox");
 		}
 		driver=new RemoteWebDriver(url,cap);
-		//driver.get("https://www.flipkart.com");
-	}
-	@Test
-	public void launchApplication()
-	{
-		driver.get("https://www.flipkart.com");
+		driver.get("http://www.flipkart.com");
+		Thread.sleep(2000);
+		driver.close();
 	}
 
 }
